47f23ab9c86f2b967a0e58bc6d9e19471143f917
vabrea <99304763+vabrea@users.noreply.github.com>
2022-03-01 18:14:00 -0800

    First Version

---
+++ a1script.js
@@ -0,0 +1,20 @@
+let buyerFirstname="Von"
+let buyerLastname="Abrea"
+let buyerAddress="1234 Street"
+let buyerCity="Ventura"
+let buyerState="CA"
+let buyerZip="93004"
+let sellerName="Amazon.com"
+let orderNumber="98765"
+let orderItem="Whiteboard"
+let itemDescription="20x20in dry erase board"
+let itemQty="1"
+let itemNumber="242424"
+let shippingTime="2-3 Business Days"
+let shipArrival="March 4, 2022"
+let orderMessage="Thank you for shopping with Amazon.com"
+
+
+let template=`
+    <p>Hello</p>
+`
